2526f692d98063da581a26947c8b53f6006fa5c7
Shelton Gayles <sgbythelake@gmail.com>
2020-03-05 23:21:07 -0800

    inital commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
+
+def create_app():
+    app = Flask(__name__)
+    # CORS(app)
+
+
+    @app.route('/api')
+    @cross_origin()
+    def api():
+        return jsonify({'data': "Hello world CORS"})
+    
+    return app
